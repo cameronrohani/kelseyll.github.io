19a78bc412a9a3ea5a54e4ef5eb41e8f6bbd1435
kelseyll <kelsey.legault@gmail.com>
2016-02-03 23:27:52 -0800

    add slider(ty cam)

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(function(){
     $(window).scroll(function(){
         if ($(window).scrollTop() > 200){
             $('.arrow').addClass('arrow-fade');
@@ -7,4 +7,14 @@ $(document).ready(function(){
           $('.arrow').removeClass('arrow-fade');
         }
     });
+    var $slider = $('.slider').find('.screen');
+    var i = 0;
+    recursive();
+    setInterval(recursive, 4000);
+
+    function recursive() {
+      $slider.removeClass('showing').eq(i).addClass('showing');
+      i = (++i % $slider.length);
+    }
+
 });
