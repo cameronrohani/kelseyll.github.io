94fc3d6bd08a3f562a3232ac69197bc2c6d7f244
kelseyll <kelsey.legault@gmail.com>
2016-02-17 16:40:36 -0800

    Shayan found dumb mistakes I fix

--- js/index.js
+++ js/index.js
@@ -1,21 +1,23 @@
-$(function(){
-    $(window).scroll(function(){
-        if ($(window).scrollTop() > 200){
-            $('.arrow').addClass('arrow-fade');
-        }
-        else {
-          $('.arrow').removeClass('arrow-fade');
-        }
-    });
-    var $slider = $('.slider').find('.screen');
-    var i = 0;
-    recursive();
-    setInterval(recursive, 4000);
-
-    function recursive() {
-      $slider.removeClass('showing').eq(i).addClass('showing');
-      i = (++i % $slider.length);
+$(window).scroll(function(){
+    if ($(window).scrollTop() > 200){
+        $('.arrow').addClass('arrow-fade');
     }
+    else {
+      $('.arrow').removeClass('arrow-fade');
+    }
+});
+var $slider = $('.slider').find('.screen');
+var i = 0;
+recursive();
+setInterval(recursive, 4000);
 
-
+function recursive() {
+  $slider.removeClass('showing').eq(i).addClass('showing');
+  i = (++i % $slider.length);
+}
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 1000);
+    return false;
 });
